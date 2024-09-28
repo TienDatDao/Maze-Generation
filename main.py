087757b62d3b93ee484e0ce3fb1d73529750dda2
Dao Tien Dat <94846751+TienDatDao@users.noreply.github.com>
2024-09-28 22:29:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,61 +2,51 @@ import pygame
 import color
 import numpy as np
 import random
+import sys
+
+sys.setrecursionlimit(2000)
 
 pygame.init()
 
-BLOCK = 10
-WIDTH, HEIGHT = BLOCK*21, BLOCK*21
+
+BLOCK = 20
+BLOCK_WIDTH = 51
+BLOCK_HEIGHT = 51
+WIDTH, HEIGHT = BLOCK*BLOCK_WIDTH, BLOCK*BLOCK_HEIGHT
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Draw circle")
 FPS = 60
 
-arr_check = [[0 for _ in range(int(WIDTH/BLOCK))] for _ in range(int(HEIGHT/BLOCK))]
-
-# save wall cells
-vector_wall = []  
-
-# save par path cells
-vector_path = [[[-1, -1] for _ in range(int(WIDTH/BLOCK))] for _ in range(int(HEIGHT/BLOCK))]
-
-# def delete_wall_cell(x, y):
-    
+arr_check = [[0 for _ in range(int(WIDTH/BLOCK)+5)] for _ in range(int(HEIGHT/BLOCK)+5)]
 
 def create_table():
-    for i in range(0, int(WIDTH/BLOCK)):
-        arr_check[i][0] = 1
-    
-    for j in range(0, int(HEIGHT/BLOCK)):
-        arr_check[0][j] = 1
-
-    for i in range(0, int(WIDTH/BLOCK)):
-        arr_check[i][int(WIDTH/BLOCK)-1] = 1
-    
-    for j in range(0, int(HEIGHT/BLOCK)):
-        arr_check[int(WIDTH/BLOCK)-1][j] = 1
-
     for i in range(1, int(WIDTH/BLOCK)-1):
         for j in range(1, int(HEIGHT/BLOCK)-1):
             if i % 2 == 1 and j % 2 == 1:
-                arr_check[i][j] = 0
-                vector_path[i][j] = [i, j]
-            else:
                 arr_check[i][j] = 1
-                vector_wall.append([i, j])
 
+def delete_wall(x, y, x_, y_):
+    if x == x_:
+        arr_check[x][int((y+y_)/2)] = 2
+    else:
+        arr_check[int((x+x_)/2)][y] = 2
 
-def random_delete_wall():
-    while len(vector_wall):
+def dfs(par_x, par_y, x, y):
+    arr_d = [[0,2], [0, -2], [2, 0], [-2, 0]]
+    check = True
+    while check and len(arr_d) > 0:
+        pos = random.randint(0, len(arr_d) - 1)
+        x_ = x + arr_d[pos][0]
+        y_ = y + arr_d[pos][1]
+        if x_ > 0 and x_ < int(WIDTH/BLOCK)-1 and y_ > 0 and y_ < int(HEIGHT/BLOCK)-1 and arr_check[x_][y_] == 1:
+            delete_wall(x, y, x_, y_)
+            arr_check[x_][y_] = 2
+            dfs(x, y, x_, y_)
+            # check = False
+        arr_d.pop(pos)
 
-        pos = random.randint(0,len(vector_wall)-1)
-        x = vector_wall[pos][0]
-        y = vector_wall[pos][1]
-        vector_wall.pop(pos)
-
-        # if delete_wall_cell(x, y):
-            # arr_check[x][y] = 0
-
+    
 
 def draw_window():
     WIN.fill(color.BLACK)
@@ -67,12 +57,14 @@ def draw_grid():
             x = i*BLOCK
             y = j*BLOCK
             if arr_check[i][j] == 0:
-                pygame.draw.rect(WIN, color.WHITE, (x, y, BLOCK, BLOCK))
-            else:
                 pygame.draw.rect(WIN, color.BLACK, (x, y, BLOCK, BLOCK))
+            else:
+                pygame.draw.rect(WIN, color.WHITE, (x, y, BLOCK, BLOCK))
 
 def main():
     create_table()
+    arr_check[1][1] = 2
+    dfs(1, 1, 1, 1)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -82,7 +74,7 @@ def main():
                 run = False
 
         draw_window()
-        random_delete_wall()
+
         draw_grid()
         
         pygame.display.flip()
